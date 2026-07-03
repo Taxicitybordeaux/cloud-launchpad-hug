d52b8b0493433511da556a28609ac246526cf978
familytech33 <familytech33@hotmail.com>
2026-07-03 17:50:53 +0200

    Restore PWA service worker

---
+++ public/sw.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+  if (url.pathname === '/sw.js') return;
+  event.respondWith(fetch(event.request));
+});
